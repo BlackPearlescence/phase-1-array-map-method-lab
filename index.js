8bf886e2a66acfacf604077d2dbce9ebf1c67bd0
Sani Rogers <sanirogers138@gmail.com>
2023-01-11 16:29:09 -0500

    done

--- index.js
+++ index.js
@@ -11,6 +11,36 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const exclusionList = ["OO", "API", "NaN","stopPropagation","PreventDefault","JSONP?"];
+const weirdList = ["Oo","Api","Nan","Stoppropagation","Preventdefault","Jsonp?"];
+
 const titleCased = () => {
+  // for(const i = 0; i < tutorials.length - 1; i++){
+  //   let splitTutorials = String.prototype.split(tutorials[i]);
+  //   for(const j = 0; j < tutorials.length - 1; j++){
+  //     splitTutorials.map(splitTutorials[0][1] + splitTutorials.slice(1));
+  //   }
+  // }
+  let words = "";
+  let count = 0;
+  // for(const tutorialTitle of tutorialList){
+  for(let i = 0; i < tutorials.length; i++){
+    // words = tutorials[i].toLowerCase();
+    words = tutorials[i].split(' ');
+    words = words.map(words => words[0].toUpperCase() + words.slice(1));
+    for(let j = 0; j < exclusionList.length ; j++){
+      for(let k = 0; k < words.length ; k++){
+        if(words[k] === weirdList[j]){
+          words[k] = exclusionList[j];
+        }
+      }
+      
+    }
+    words = words.join(" ");
+    tutorials[i] = words;
+  }
+  
+  // }
+  
   return tutorials
-}
+}     
